1cab62b774a170f5f14cf36217588684f92fd19f
2yman <ayman_20140114@fci.helwan.edu.eg>
2020-09-13 19:08:49 +0200

    finsh home page

---
+++ app/controllers/Profile.php
@@ -0,0 +1,14 @@
+<?php
+
+class Profile extends Controller {
+
+    public function __construct($controller, $action) {
+        parent::__construct($controller, $action);
+      }
+
+    public function indexAction() {
+     
+        $this->view->render('home/index');
+
+      }
+    }
--- app/views/layouts/profile.php
+++ app/views/layouts/profile.php
@@ -23,7 +23,7 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-      <a class="nav-link" href="<?php echo PROOT?>Profile">Profile <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="<?php echo PROOT?>Profile">Profile <span class="sr-only">(current)</span></a>
       </li>
       </ul>
     <?php
